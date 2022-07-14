5eb1c2a14c325b42d583b26c3b6ad233c1b8f3e9
hiteshdureja <78369894+hiteshdureja@users.noreply.github.com>
2022-07-15 05:15:36 +0530

    Time: 3 ms (75.18%), Space: 9.2 MB (29.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        auto ins_idx = nums1.size();
-        while(ins_idx--)
-            if(m == 0)
-                nums1[ins_idx] = nums2[--n];
-            else if(n == 0)
-                nums1[ins_idx] = nums1[--m];
-            else if(nums1[m - 1] <= nums2[n - 1])
-                nums1[ins_idx] = nums2[--n];
-            else
-                nums1[ins_idx] = nums1[--m];
+    void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
+        m+=n;
+        for(int i=0;i<m;i++){
+            if(i>=(m-n)){
+                arr1[i]=arr2[m-i-1];
+            }
+        }
+        sort(arr1.begin(),arr1.end());
     }
 };
