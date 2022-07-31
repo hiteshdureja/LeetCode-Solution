9df4f166eebdc0d570a48cd81bf39c4a18bf6532
hiteshdureja <78369894+hiteshdureja@users.noreply.github.com>
2022-07-31 16:45:11 +0530

    Time: 7 ms (9.55%), Space: 6.3 MB (61.99%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -3,27 +3,27 @@ public:
     int strStr(string haystack, string needle) {
         int n = haystack.length();
         int m =needle.length();
-        int s=0;
+        int l=0;
         int f=0;
         for(int i=0;i<n;i++){
-            if(haystack[i] == needle[s]){
+            if(haystack[i] == needle[l]){
                 int a = i;
-                while(s<m && a<n){
-                    if(haystack[a] == needle[s]){
+                while(l<m && a<n){
+                    if(haystack[a] == needle[l]){
                         a++;
-                        s++;
+                        l++;
                         f=1;
                     }
-                    else if(haystack[a] != needle[s]){
-                        s=0;
-                        f=0;
+                    else if(haystack[a] != needle[l]){
+                        l=f=0;
                         break;
                     }
                 }
-                if(a==n && s<m)
+                if(a==n and l<m)
                     f=0;
-                if(f==1) return i;
-                s=0;
+                if(f==1) 
+                    return i;
+                l=0;
             }
         }
         return -1;
