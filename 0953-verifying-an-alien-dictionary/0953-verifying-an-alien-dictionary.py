7ab8e9271fbfdf908aa9cf45b6b2610c9a03b63e
hiteshdureja <78369894+hiteshdureja@users.noreply.github.com>
2023-06-06 13:18:49 +0530

    Time: 7 ms (46.37%), Space: 9.7 MB (56.13%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+    unordered_map<char,int>mp;
+        for(int i=0;i<order.length();i++) 
+            mp[order[i]]=i;
+        int n=words.size();
+        for(int i=0;i<n-1;i++)
+        {
+            string first=words[i];
+            string second=words[i+1];
+            bool flag=false;
+            for(int j=0;j<min(first.length(),second.length());j++)
+            {
+                if(mp[first[j]]< mp[second[j]])
+                {  
+                     flag=true;
+                    break;
+                }
+                else if(mp[first[j]]==mp[second[j]]) continue;
+                else return false;
+            }
+            if(first.length()>second.length() && !flag) 
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+};
