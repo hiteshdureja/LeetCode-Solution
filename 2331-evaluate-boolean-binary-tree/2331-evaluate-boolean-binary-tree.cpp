5034b8e548d576f07cd0062f9dab93bfa23ce15a
Hitesh Dureja <78369894+hiteshdureja@users.noreply.github.com>
2024-05-17 05:02:02 +0530

    Time: 3 ms (99.53%), Space: 17.4 MB (64.62%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -14,7 +14,7 @@ public:
     bool evaluateTree(TreeNode* root) {
         if(root -> val == 1) 
             return true;
-        if(root -> val == 0)
+        else if(root -> val == 0)
             return false;
         else if(root -> val == 2) 
             return evaluateTree(root -> left) || evaluateTree(root -> right);
